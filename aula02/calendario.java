7551ac6f0a457ed5b07ed07b5f302f7f6b11390a
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-03-16 21:02:19 -0300

    Update calendario.java

---
+++ aula02/calendario.java
@@ -0,0 +1,14 @@
+public class calendario {
+    // Função principal
+    public static void main(String[] args){
+        // Mostrar uma mensagem para o usuario
+        System.out.println("Digite o mês:"); 
+
+        // depois quero ler o numero digitado no teclado 
+
+        //e depois quero mostrar o nome do mes.
+        // Com IF
+        // Com Switch
+
+    }
+}
