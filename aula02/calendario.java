5d2bf2ff9dd0895757d551980337c2316cbe6731
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-03-16 21:30:32 -0300

    Update calendario.java

--- aula02/calendario.java
+++ aula02/calendario.java
@@ -1,14 +1,21 @@
+import java.util.Scanner;
+
 public class calendario {
     // Função principal
     public static void main(String[] args){
         // Mostrar uma mensagem para o usuario
         System.out.println("Digite o mês:"); 
+        Scanner input = new Scanner(System.in);
+        int mes = input.nextInt();
 
-        // depois quero ler o numero digitado no teclado 
+        // Depois quero ler o numero digitado no teclado 
 
-        //e depois quero mostrar o nome do mes.
+
+        // E depois quero mostrar o nome do mes.
         // Com IF
         // Com Switch
+        System.out.println("Numero digitado = " + mes);
+        input.close();
 
     }
 }
