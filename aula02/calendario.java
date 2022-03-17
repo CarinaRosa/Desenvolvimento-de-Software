606b951990cfc87dcd2c16211a00688097d7bbdd
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-03-16 21:46:23 -0300

    Update calendario.java

--- aula02/calendario.java
+++ aula02/calendario.java
@@ -1,15 +1,44 @@
 import java.util.Scanner;
 
 public class calendario {
+    //Funções procedimentos
+    public static String mostrarNomeMesIf(int mes){
+        String nomeMes = "";
+        if (mes == 1) {
+            nomeMes = "Janeiro";
+        } else if (mes == 2) {
+            nomeMes = "Fevereiro";
+        } else if (mes == 3) {
+            nomeMes = "Março";
+        } else if (mes == 4) {
+            nomeMes = "Abril";
+        } else if (mes == 5) {
+            nomeMes = "Maio";
+        } else if (mes == 6) {
+            nomeMes = "Junho";
+        } else if (mes == 7) {
+            nomeMes = "Julho";
+        } else if (mes == 8) {
+            nomeMes = "Agosto";
+        } else if (mes == 9) {
+            nomeMes = "Setembro";
+        } else if (mes == 10) {
+            nomeMes = "Outubro";
+        } else if (mes == 11) {
+            nomeMes = "Novembro";
+        } else if (mes == 12) {
+            nomeMes = "Dezembro";
+        } 
+        return nomeMes;
+    }
     // Função principal
     public static void main(String[] args){
         // Mostrar uma mensagem para o usuario
         System.out.println("Digite o mês:"); 
         Scanner input = new Scanner(System.in);
-        int mes = input.nextInt();
-
+       
         // Depois quero ler o numero digitado no teclado 
-
+        int mes = input.nextInt();
 
         // E depois quero mostrar o nome do mes.
         // Com IF
