f7cf7843c5092fa67339961dd38783fbe1e73af5
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-03-23 19:41:35 -0300

    Create sorteio.java

---
+++ aula03/sorteio.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class sorteio {
+
+    // Atributos, propriedades, variaveis globais
+    // Funções e procedimentos
+    // Rotina principal
+    public static void main(String[] args) {
+        Random gerador = new Random(); 
+        int n = 1 + gerador.nextInt(100);
+    }
+}
