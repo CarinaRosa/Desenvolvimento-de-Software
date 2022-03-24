4d2ef646a870fa2f18bca1123df084f9cf043585
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-03-23 22:12:24 -0300

    Create ParImpar.java

---
+++ aula03/ParImpar.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ParImpar{
+    public static String verificarParImpar(int n){
+        if(n % 2 == 0){
+            return "Par";
+        }
+
+        else {
+            return "Impar";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int n;
+
+        do{
+            n = teclado.nextInt();
+            if (n>=0) {
+                System.out.printIn(verificaParImpar(n));
+            }
+        }
+        while(n >= 0){
+            teclado.close();
+        }
+    }
+    
+}
