5e026352f459b7f01631ff4c4443668b575a8061
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-03-23 21:23:15 -0300

    Update sorteio.java

--- aula03/sorteio.java
+++ aula03/sorteio.java
@@ -8,16 +8,41 @@ public class sorteio {
     public static void main(String[] args) {
         Random gerador = new Random(); 
         int c = 1;
-        
+        int maior = 0;
+        int menor = 0;
+        int soma = 0;
+
         while (c <= 1000) {
             //ler o numero
             int n = 1 + gerador.nextInt(100);
-            c++; // c +=1 ou c=c+1;
+            
             System.out.println(n);
-        }
-        
 
-       
+            if (c == 1) {
+                maior = n;
+                menor = n;
+            }
+            
+            soma = soma + n;        
+            
+            if (maior < n) {
+                maior = n;                
+            }                       
+          
+            if (menor > n) {
+                menor = n;                
+            }
+
+            c++; // c +=1 ou c=c+1;            
+                               
+        }
+    
+        System.out.println("O maior é:" + maior);
+        System.out.println("O menor é:" + menor);
+        System.out.println("media=" + soma/1000);
+     
+      
+      
     }
 
 }
