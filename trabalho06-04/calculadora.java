d268928e12605251e4cae3e6c67cbab01e6d763c
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-03-27 22:17:16 -0300

    Create calculadora.java

---
+++ trabalho06-04/calculadora.java
@@ -0,0 +1,24 @@
+public class calculadora{
+    public static calcular(int a, int b){
+
+    }
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int a;
+        int b;
+
+        public static int operacao(int a, int b)
+
+        a = teclado.nextInt();
+        b = teclado.nextInt();
+
+        
+
+        
+        teclado.close();
+              
+    
+    }
+}
+
