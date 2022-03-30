a311b6b357ba55c59ffd6d5d69fcf08b15e65fcd
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-03-30 19:59:57 -0300

    update

--- aula04/tabuada.java
+++ aula04/tabuada.java
@@ -1,5 +1,12 @@
 public class tabuada {
+
     public static void main(String[] args) {
         
+       for (int i = 0; i <= 10; i++) {           
+            for (int j = 0; j <= 10; j++) {
+                System.out.printf("%d x %d = %d\n", i, j, i * j);
+            }
+        }
     }
+    
 }
--- trabalho06-04/calculadora.java
+++ trabalho06-04/calculadora.java
@@ -2,49 +2,52 @@ import java.util.Scanner;
 
 public class calculadora{             
 
-    public static Void main(String[] args) {
+    public static void main(String[] args) {
 
+        Scanner teclado = new Scanner(System.in); 
         System.out.println("Digite o primeiro numero inteiro: ");
-        System.out.println("Digite o segundo numero inteiro: ");
-        System.out.println("Digite a operação desejada (1: soma, 2: subtração, 3: multiplicação e 4: divisão): ");
-        Scanner teclado = new Scanner(System.in);
-        
         int a = teclado.nextInt();
+        System.out.println("Digite o segundo numero inteiro: ");
         int b = teclado.nextInt();
-        int numero = teclado.nextInt();
+        System.out.println("Digite a operação desejada (1-Soma, 2-Subtração, 3-Multiplicação e 4-Divisão): ");
+        int numero = teclado.nextInt();                  
+          
                
-        teclado.close();
-            
+        teclado.close();            
         
-        int resultado;
+        
         if(numero == 1){
-            resultado = soma(a, b);
-        
+             soma(a, b);
+             System.out.println("Resultado é: " + soma(a, b));
+                     
         }else if(numero == 2){
-            resultado = subtracao(a, b);
+             subtracao(a, b);
+             System.out.println("Resultado é: " + subtracao(a, b));
 
         }else if(numero == 3){
-            resultado = multiplicacao(a, b);
+            multiplicacao(a, b);
+            System.out.println("Resultado é: " + multiplicacao(a, b));
 
         }else if(numero == 4){
-            resultado = divisao(a, b);
+            divisao(a, b);
+            System.out.println("Resultado é: " + divisao(a, b));
         }
 
         else{
             System.out.println("Operação invalida! Digite 1 para soma, 2 para subtração, 3 para multiplicação e 4 para divisão.");
         }    
 
-        
+        /*
+        System.out.println("Primeiro numero digitado: " + a);
+        System.out.println("Segundo numero digitado: " + b);
+        System.out.println("Resultado da operação:" );
+        */
                  
 
     }
-     /*
-    Void resultado;
-
-        System.out.println("Primeiro numero digitado: " + a);
-        System.out.println("Segundo numero digitado: " + b);
-        System.out.println("Resultado da operação:" + resultado);
-    */
+     
+      
+    
     public static int soma(int a, int b){
         int resultado = a + b;
         return resultado;
