7c2c7da194329cf7245c5aa0f924e2dcee8e07c6
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-03-30 20:29:55 -0300

    Update tabuada.java

--- aula04/tabuada.java
+++ aula04/tabuada.java
@@ -2,8 +2,8 @@ public class tabuada {
 
     public static void main(String[] args) {
         
-       for (int i = 0; i <= 10; i++) {           
-            for (int j = 0; j <= 10; j++) {
+       for (int i = 1; i <= 10; i++) {           
+            for (int j = 1; j <= 10; j++) {
                 System.out.printf("%d x %d = %d\n", i, j, i * j);
             }
         }
