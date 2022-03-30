3c06327d34cc688866c318947ae5be8789ae8560
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-03-29 21:16:51 -0300

    Update calculadora.java

--- trabalho06-04/calculadora.java
+++ trabalho06-04/calculadora.java
@@ -1,24 +1,39 @@
 public class calculadora{
-    public static calcular(int a, int b){
+    public static int operacao(int a, int b){
 
-    }
-
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int a;
         int b;
-
-        public static int operacao(int a, int b)
-
-        a = teclado.nextInt();
-        b = teclado.nextInt();
-
+        int operacao;
+        int resultado;
         
+        if(operacao === 1){
+            resultado = a + b;
+        
+        }else if(operacao === 2){
+            resultado = a - b;
+
+        }else if(operacao === 3){
+            resultado = a * b;
+
+        }else if(operacao === 4){
+            resultado = a / b;
+        }
+
+        else{
+            System.out.printIn("Operação invalida! Digite 1 para soma, 2 para subtração, 3 para multiplicação e 4 para divisão.")
+        }    
 
         
         teclado.close();
               
     
     }
+
+    }
+
+    System.out.printIn(resultado);
+    
 }
 
