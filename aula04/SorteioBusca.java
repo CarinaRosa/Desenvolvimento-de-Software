a0640cc70f6f1d7ced0cc063ca56a82b3f528656
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-03-30 21:35:33 -0300

    Create SorteioBusca.java

---
+++ aula04/SorteioBusca.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class SorteioBusca {
+
+    //Atributos
+    public static Scanner teclado;
+    public static int[] vetor;
+
+    //Funções e procedimentos
+    public static void sortear(){
+        Random gerador = new Random();
+        vetor = new int[20];
+
+        for (int i = 0; i < vetor.length; i++) {
+            
+        }
+    }
+
+    public static int procurar(int n){}
+
+    //Rotinas principais
+    public static void main(String[] args) {
+        
+    }
+
+}
