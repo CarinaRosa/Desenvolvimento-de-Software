f519d2187d42a6ba691711e859cd8d80ec57c1f5
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-03-30 21:52:00 -0300

    Update SorteioBusca.java

--- aula04/SorteioBusca.java
+++ aula04/SorteioBusca.java
@@ -13,15 +13,36 @@ public class SorteioBusca {
         vetor = new int[20];
 
         for (int i = 0; i < vetor.length; i++) {
-            
+            vetor[i] = gerador.nextInt(100) +1;
         }
     }
 
-    public static int procurar(int n){}
+    public static int procurar(int n){
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] == n) {
+                return i;
+                
+            }
+        }
+        return -1;
+    }
 
     //Rotinas principais
     public static void main(String[] args) {
-        
+        sortear();
+        teclado = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        int num = teclado.nextInt();
+        int pos = procurar(num);
+
+        if (pos >= 0) {
+            System.out.printf("Numero encontrado na posição %d", pos);
+            
+        } else{
+            System.out.println("Numero não encontrado.");
+        }
+
+        teclado.close();
     }
 
 }
