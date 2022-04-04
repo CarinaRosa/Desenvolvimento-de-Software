7edb38ef588521b02a0debd0dba4033c7bcbab20
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-04-04 15:44:16 -0300

    commit

---
+++ Fibonacci/fibonacci.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class fibonacci {  
+
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        int f = teclado.nextInt();
+
+        teclado.close();
+
+        long n1 = 0, n2 = 1;
+
+        if (f == 0) {
+            System.out.println("o enesimo numero é = 0.");
+        }else if (f == 1) {
+            System.out.println("o enesimo numero é = 1.");
+        }else if (f < 0) {
+            System.out.println("Insira um numero positivo");
+        }else {
+            for (int i = f; i > 0; i--) {
+                System.out.print(n1 + " ");
+                long n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+                           
+                
+            }
+            System.out.println("Este é o n-esimo numero.");
+        }
+        
+    }
+}
