114a11ecf61a7ef166daee0a4cd2d10eb1c7ee86
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-04-05 13:50:47 -0300

    Update jogo.java

--- Jogo_Adivinha/jogo.java
+++ Jogo_Adivinha/jogo.java
@@ -3,42 +3,41 @@ import java.util.Scanner;
 
 public class jogo {
     //Atributos
-    public static Scanner teclado;
-    public static int[] vetor;
-
-    //Funções e procedimentos
-    public static void adivinhar(){
-        Random gerador = new Random();
-        vetor = new int[10];
-
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = gerador.nextInt(100) +1;
-        }
-    }
-
-    public static int procurar(int n){
-        for (int i = 0; i < vetor.length; i++) {
-            if (vetor[i] == n) {
-                return i;
-            }else if (vetor[i] > n) {
-                System.out.println("O numero sorteado é maior do que o digitado.");
-            }else {
-                System.out.println("O numero sorteado é menor do que o digitado.");
-            }
-
-        }
-        return -1;
-    }
-    
     //Rotinas principais
 
     public static void main(String[] args) {
-        adivinhar();
-        teclado = new Scanner(System.in);
-        System.out.println("Digite um numero: ");
-        int num = teclado.nextInt();
-        int pos = procurar(num);
-        int tentativas = 10;
         
+        Scanner teclado = new Scanner(System.in);
+        Random gerador = new Random();
+
+        boolean acerto = false;
+        int tentativa = 10;
+        int numSorteado = gerador.nextInt(100) +1;
+        long adivinha = 0;
+
+        //System.out.println(numSorteado);
+
+        while (tentativa > 0 && acerto ==  false) {
+            System.out.println("Digite um numero: ");
+            adivinha = teclado.nextLong();
+
+            if (numSorteado == adivinha) {
+                System.out.println("Parabens, você acertou!! ");
+                acerto = true;
+            } else if (adivinha < numSorteado) {
+                --tentativa;
+                System.out.println("Numero informado muito baixo, tente um numero maior. Você possui " + tentativa + " tentativas restantes.");                
+            } else {
+                --tentativa;
+                System.out.println("Numero informado muito alto, tente um numero menor. Você possui " + tentativa + " tentativas restantes.");
+            }
+
+            if (tentativa == 0) {
+                System.out.println("Suas tentativas acabaram e você não conseguiu acertar o numero.");
+            }
+        }
+
+        teclado.close();
+
     }
 }
