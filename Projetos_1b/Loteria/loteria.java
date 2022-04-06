8bcb50d8f10e2586eeb421237849ab4b74a29aca
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-04-05 21:53:01 -0300

    Update loteria.java

--- Projetos_1b/Loteria/loteria.java
+++ Projetos_1b/Loteria/loteria.java
@@ -1,5 +1,20 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class loteria {
     public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        Random gerador = new Random();
+
         
+        int jogada1 = 0;
+        int jogada2 = 0;
+        int jogada3 = 0;
+        int jogada4 = 0;
+        int jogada5 = 0;
+        int jogada6 = 0;
+
+
     }
 }
