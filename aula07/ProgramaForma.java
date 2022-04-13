ac8e33225eacda23b63d57c3c1f6531e918d12f6
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-04-13 20:32:25 -0300

    Update ProgramaForma.java

--- aula07/ProgramaForma.java
+++ aula07/ProgramaForma.java
@@ -1,5 +1,12 @@
 public class ProgramaForma {
     public static void main(String[] args) {
-        
+        FormaGeometrica obj1 = new FormaGeometrica();
+        System.out.println(obj1 + " - " + obj1.getCorPreenchimento());
+        Circulo obj2 = new Circulo();
+        System.out.println(obj2 + " - " + obj2.getCorPreenchimento());
+        Retangulo obj3 = new Retangulo();
+        System.out.println(obj3 + " - " + obj3.getCorPreenchimento());
+        Triangulo obj4 = new Triangulo();
+        System.out.println(obj4 + " - " + obj4.getCorPreenchimento());
     }
 }
