ef6251cf0da62df3c14136529b80c379635e83a5
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-04-20 20:14:41 -0300

    Update Ponto.java

--- Aula07_Aluno/Ponto.java
+++ Aula07_Aluno/Ponto.java
@@ -3,10 +3,8 @@ public class Ponto {
     private double x;
     private double y;
 
-    public Ponto(){
-        
+    public Ponto(){        
     }
-
     public Ponto(double x, double y){
         this.x = x;
         this.y = y;
@@ -21,15 +19,12 @@ public class Ponto {
     public double getx(){
         return x;
     }
-
     public double gety(){
         return y;
     }
-
     public void setx(double x){
         this.x = x;
     }
-
     public void sety(double y){
         this.y = y;
     }
@@ -37,7 +32,6 @@ public class Ponto {
     public double distancia(){
         return Math.sqrt(Math.pow(x, 2) + Math.pow(x, 2));
     }
-
     public double distancia(Ponto p){
         return Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2));
     }
