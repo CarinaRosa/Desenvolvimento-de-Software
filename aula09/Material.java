926c37ab5037dbcfd8d22082fabb967d873b9b34
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-05-04 21:34:38 -0300

    up

---
+++ aula09/Material.java
@@ -0,0 +1,35 @@
+public class Material {
+
+    private int codigo;
+    private String titulo;
+
+    public Material(){
+
+    }
+
+    public Material(int codigo, String titulo){
+        this.titulo = titulo;
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Codigo: %d\nTitulo: %s\n", this.codigo, this.titulo);
+    } 
+}
