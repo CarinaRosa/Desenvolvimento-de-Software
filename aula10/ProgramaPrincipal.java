a68ef28ced2d8d38391a4defad61f55f715109bc
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-05-11 20:08:36 -0300

    Update ProgramaPrincipal.java

--- aula10/ProgramaPrincipal.java
+++ aula10/ProgramaPrincipal.java
@@ -2,8 +2,14 @@ package aula10;
 
 public class ProgramaPrincipal {
     public static void main(String[] args) {
-        FormaGeometrica forma = new FormaGeometrica();{
-            
+        FormaGeometrica vetor[] = new FormaGeometrica[3];
+
+        vetor[0] = new Circulo(10);
+        vetor[1] = new Retangulo(2.5, 3);
+        vetor[2] = new Triangulo(3, 6);
+
+        for (FormaGeometrica f : vetor) {
+            System.out.printf("%s -> area: %f\n", f.getClass().getName(), f.calcularArea());
         }
     }
 }
