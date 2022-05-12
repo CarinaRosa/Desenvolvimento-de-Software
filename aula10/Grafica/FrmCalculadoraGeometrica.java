9f537037aff4b71bb04a255b84e9a760746626aa
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-05-11 21:24:15 -0300

    up

---
+++ aula10/Grafica/FrmCalculadoraGeometrica.java
@@ -0,0 +1,11 @@
+package aula10.Grafica;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class FrmCalculadoraGeometrica extends JFrame {
+    public FrmCalculadoraGeometrica(){
+        setSize(new Dimension(720, 600));
+    }
+}
--- aula10/Logica/ProgramaPrincipal.java
+++ aula10/Logica/ProgramaPrincipal.java
@@ -1,15 +1,11 @@
 package aula10.Logica;
 
+import aula10.Grafica.FrmCalculadoraGeometrica;
+
 public class ProgramaPrincipal {
     public static void main(String[] args) {
-        FormaGeometrica vetor[] = new FormaGeometrica[3];
+       FrmCalculadoraGeometrica frm = new FrmCalculadoraGeometrica();
 
-        vetor[0] = new Circulo(0.5);
-        vetor[1] = new Retangulo(2.5, 3);
-        vetor[2] = new Triangulo(3, 6);
-
-        for (FormaGeometrica f : vetor) {
-            System.out.printf("%s -> area: %f -> cor: %s\n", f.getClass().getName(), f.calcularArea(), f.preencherCor());
-        }
+       frm.setVisible(true);
     }
 }
