993e3940e26d8905b86d8c28324c5a660324dd98
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-05-18 19:37:31 -0300

    Update ActionCalcularArea.java

--- aula10/Grafica/ActionCalcularArea.java
+++ aula10/Grafica/ActionCalcularArea.java
@@ -1,7 +1,11 @@
 package aula10.Grafica;
 
+import java.awt.event.ActionEvent;
+
 import javax.swing.AbstractAction;
 import javax.swing.JTextField;
+
+import aula10.Logica.Circulo;
 
 public class ActionCalcularArea extends AbstractAction{
     public enum OpcaoForma{
@@ -19,10 +23,26 @@ public class ActionCalcularArea extends AbstractAction{
     public ActionCalcularArea(JTextField txtRaio, OpcaoForma opcaoForma, JTextField txtArea){
         this.txtRaio = txtRaio;
         this.opcaoForma = opcaoForma;
+        this.txtArea = txtArea;
     }
     public ActionCalcularArea(JTextField txtBase, JTextField txtAltura, OpcaoForma opcaoForma, JTextField txtArea){
         this.txtBase = txtBase;
-        this.txtAltura = txtAltura;    
+        this.txtAltura = txtAltura;
+        this.opcaoForma = opcaoForma;
+        this.txtArea = txtArea;    
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (this.opcaoForma) {
+            case CIRCULO:
+                double raio = Double.parseDouble(txtRaio.getText());
+                Circulo circulo = new Circulo(raio);
+                double area = circulo.calcularArea();
+                txtArea.setText(String.format("%f", area));
+                break;        
+            
+        }
+        
     }
 
 }
