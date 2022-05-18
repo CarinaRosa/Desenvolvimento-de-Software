0994b672f9fd2c881bdba2180d7f3dda3d815fc4
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-05-18 19:31:30 -0300

    Update ActionCalcularArea.java

--- aula10/Grafica/ActionCalcularArea.java
+++ aula10/Grafica/ActionCalcularArea.java
@@ -4,9 +4,25 @@ import javax.swing.AbstractAction;
 import javax.swing.JTextField;
 
 public class ActionCalcularArea extends AbstractAction{
+    public enum OpcaoForma{
+        CIRCULO,
+        RETANGULO,
+        TRIANGULO
+    }
+
     private JTextField txtRaio;
     private JTextField txtBase;
     private JTextField txtAltura;
+    private JTextField txtArea;
+    private OpcaoForma opcaoForma;
 
-    
+    public ActionCalcularArea(JTextField txtRaio, OpcaoForma opcaoForma, JTextField txtArea){
+        this.txtRaio = txtRaio;
+        this.opcaoForma = opcaoForma;
+    }
+    public ActionCalcularArea(JTextField txtBase, JTextField txtAltura, OpcaoForma opcaoForma, JTextField txtArea){
+        this.txtBase = txtBase;
+        this.txtAltura = txtAltura;    
+    }
+
 }
