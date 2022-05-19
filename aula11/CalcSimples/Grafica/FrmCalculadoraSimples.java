632ab49abc016629a37d35a670e88d5d24befe50
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-05-18 21:41:17 -0300

    Create FrmCalculadoraSimples.java

---
+++ aula11/CalcSimples/Grafica/FrmCalculadoraSimples.java
@@ -0,0 +1,12 @@
+package aula11.CalcSimples.Grafica;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class FrmCalculadoraSimples extends JFrame{
+    private JTextField txtX, txtY;
+    private JComboBox<String> cbxOperacao;
+    private JButton btnCalcular;
+}
