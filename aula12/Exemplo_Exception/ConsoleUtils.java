579eadb78ca3f2730f21f6bf2e5510cb467ea6c7
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-05-25 19:33:00 -0300

    Update ConsoleUtils.java

--- aula12/Exemplo_Exception/ConsoleUtils.java
+++ aula12/Exemplo_Exception/ConsoleUtils.java
@@ -1,4 +1,19 @@
 package aula12.Exemplo_Exception;
+
+import java.util.Scanner;
+
 public class ConsoleUtils {
-    
+
+    public static int lerInteiro(String msg){
+        System.out.println(msg);
+        Scanner input = new Scanner(System.in);
+        String linhaDigitada = input.next();
+        try {
+            int num = Integer.parseInt(num);
+            return num;
+        } catch (NumberFormatException ex) {
+            System.out.println("Valor informado não é um numero inteiro valido. Tente novamente!");
+        }
+        
+    }
 }
