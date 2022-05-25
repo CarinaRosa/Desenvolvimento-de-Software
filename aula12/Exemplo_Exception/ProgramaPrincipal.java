a6389edab2ca43c1e26a0205ee1623004d6f8e9d
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-05-25 20:44:15 -0300

    Update ProgramaPrincipal.java

--- aula12/Exemplo_Exception/ProgramaPrincipal.java
+++ aula12/Exemplo_Exception/ProgramaPrincipal.java
@@ -2,7 +2,19 @@ package aula12.Exemplo_Exception;
 
 public class ProgramaPrincipal {
     public static void main(String[] args) {
+
+        //Para rodar o ConsoleUtils
+        /*int num = ConsoleUtils.lerInteiro("Digite um inteiro:");
+        System.out.printf("Inteiro digitado: %d", num);*/
+
+
+        //Para rodar o Fatorial
         int num = ConsoleUtils.lerInteiro("Digite um inteiro:");
-        System.out.printf("Inteiro digitado: %d", num);
+        long fat = Fatorial.calcular(num);
+        if (fat == -1) {
+            System.out.println("Não foi possivel calcular!");
+        } else {
+            System.out.printf("\n%d! = %d", num, fat);
+        }
     }
 }
