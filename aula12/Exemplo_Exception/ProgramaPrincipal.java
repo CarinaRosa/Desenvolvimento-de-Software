59351664a5794186b590b226a8ab872bfa941542
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-05-25 21:36:47 -0300

    Update ProgramaPrincipal.java

--- aula12/Exemplo_Exception/ProgramaPrincipal.java
+++ aula12/Exemplo_Exception/ProgramaPrincipal.java
@@ -20,12 +20,14 @@ public class ProgramaPrincipal {
 
         // Segunda forma:
         int num = ConsoleUtils.lerInteiro("Digite um inteiro:");
-        
+
         try {
             long fat = Fatorial.calcular(num);
             System.out.printf("%d! = %d", num, fat);
         } catch (IllegalArgumentException ex) {
             System.out.println(ex.getMessage());
+        } finally {
+            System.out.println("Finish! Volte sempre.");
         }
         
 
