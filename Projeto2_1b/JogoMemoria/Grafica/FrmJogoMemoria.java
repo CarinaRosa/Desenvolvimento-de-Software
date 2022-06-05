c0fdefccc1bade40578c599f62e6f9bb206cc7c5
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-06-05 16:48:37 -0300

    up

--- Projeto2_1b/JogoMemoria/Grafica/FrmJogoMemoria.java
+++ Projeto2_1b/JogoMemoria/Grafica/FrmJogoMemoria.java
@@ -7,6 +7,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.*;
 
 public class FrmJogoMemoria extends JFrame {
     
@@ -16,7 +17,7 @@ public class FrmJogoMemoria extends JFrame {
     int Aleatorio[] = new int [16];
     int Posicao_do_vetor_Aleatorio[] = new int [16];
     private JPanel Panel = new JPanel();
-    private GroupLayout Layout_do_Jogo = new GridLayout(4,4);
+    private GridLayout Layout_do_Jogo = new GridLayout(4,4);
     private JButton Escolha[] = new JButton[16];
  
     // private JPanel Barra_de_Status = new JPanel();
--- Projeto2_1b/JogoMemoria/Logica/ProgramaPrincipal.java
+++ Projeto2_1b/JogoMemoria/Logica/ProgramaPrincipal.java
@@ -1,11 +1,11 @@
 package Projeto2_1b.JogoMemoria.Logica;
 
-import Projeto2_1b.JogoMemoria.JogoDaMemoria;
+// import Projeto2_1b.JogoMemoria.JogoDaMemoria;
 
-public class ProgramaPrincipal {
-    public static void main(String [] args){
+// public class ProgramaPrincipal {
+//     public static void main(String [] args){
      
-        new JogoDaMemoria();
+//         new JogoDaMemoria();
      
-    }  
-}
+//     }  
+// }
